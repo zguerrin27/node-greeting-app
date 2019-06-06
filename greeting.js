58496655f34e0b2ce67351ca408fe25fc07caed1
Zachary Guerrin <39317224+zguerrin27@users.noreply.github.com>
2019-06-06 14:39:31 -0400

    Assignment-5: Submitted

---
+++ greeting.js
@@ -0,0 +1,15 @@
+exports.greetingEnglish = function(){
+  console.log("Hello");
+}
+
+exports.greetingJapanese = function(){
+  console.log("Konichiwa");
+}
+
+exports.greetingSpanish = function(){
+  console.log("Hola");
+}
+
+exports.greetingIcelandic = function(){
+  console.log("Halló");
+}
---
+++ main.js
@@ -0,0 +1,10 @@
+const greetings = require("./greeting.js");
+
+// "Hello"
+greetings.greetingEnglish();
+// "Konichiwa"
+greetings.greetingJapanese();
+// "Hola"
+greetings.greetingSpanish();
+// "Halló"
+greetings.greetingIcelandic();
